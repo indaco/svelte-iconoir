353a4c8fd8240a268efd6ed2ea9180846a5178d3
indaco <github@mircoveltri.me>
2021-12-15 01:06:01 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,9 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		package: {
-			dir: 'dist'
-		}
-	}
+			dir: 'dist',
+		},
+	},
 };
 
 export default config;
