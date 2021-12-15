8163ee06ce1e5e576b05ad21d35d71a1f8f66008
indaco <github@mircoveltri.me>
2021-12-15 01:06:27 +0100

    Update buildIconsDataset.mjs

--- scripts/buildIconsDataset.mjs
+++ scripts/buildIconsDataset.mjs
@@ -10,8 +10,8 @@ const __dirname = dirname(__filename);
 
 const INPUT_FOLDER = join(__dirname, '..', 'packages', 'iconoir', 'icons');
 const ICONS_OUTPUT_FOLDER = join(__dirname, '..', 'src', 'lib', 'iconoir');
-const DIST_FOLDER = join(__dirname, '..', 'dist');
 const INDEX_FILE = join(__dirname, '..', 'src', 'lib', 'index.ts');
+const DIST_FOLDER = join(__dirname, '..', 'dist');
 
 let counter = 0;
 
@@ -22,18 +22,6 @@ function main() {
 	makeDir(ICONS_OUTPUT_FOLDER);
 	generateIconsDataset();
 	console.info('\nIcons counter = ' + counter + '\n');
-}
-
-function resetAll(indexFilepath, outputFolder, distFolder) {
-	_removeIndexFile(indexFilepath);
-	_removeFolder(outputFolder);
-	_removeFolder(distFolder);
-}
-
-function makeDir(pathToDir) {
-	if (!fs.existsSync(pathToDir)) {
-		fs.mkdirSync(pathToDir);
-	}
 }
 
 function generateIconsDataset() {
@@ -49,27 +37,67 @@ function generateIconsDataset() {
 				icon['data'].push(child.properties);
 			});
 		});
-		counter++;
-		// write JSON string to a file
-		saveIconData(filename, JSON.stringify(icon));
+		// generate svelte component for each icon
+		makeIconComponent(ICONS_OUTPUT_FOLDER, icon);
 		// append an entry to index.js file
 		appendToExports(filename);
+		counter++;
 	});
-	appendIconoirEntryToExports();
 }
 
-function saveIconData(filename, jsonString) {
-	console.info('\n* Saving ' + filename + '.json to ' + ICONS_OUTPUT_FOLDER);
-	fs.writeFileSync(join(ICONS_OUTPUT_FOLDER, filename + '.json'), jsonString, (err) => {
+function makeIconComponent(outputFolder, iconObj) {
+	const iconFilename = _makeIconNameString(iconObj.name);
+	console.info('\n* Saving ' + iconFilename + '.svelte to ' + outputFolder);
+
+	let txt =
+		`
+<script>
+	let altText = '` +
+		iconObj.name +
+		` icon';
+	export let size = '1.5em';
+	export let color = 'currentColor';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	stroke-width={1.5}
+	viewBox="0 0 24 24"
+	fill="none"
+	{color}
+	aria-hidden="true"
+	aria-labelledby={altText}
+	class={$$props.class}
+	style={$$props.style}
+>` +
+		buildIconDataString(iconObj.data).join(' ') +
+		`</svg>`;
+
+	fs.writeFileSync(join(outputFolder, iconFilename + '.svelte'), txt, (err) => {
 		if (err) {
 			throw err;
 		}
 	});
 }
 
+function buildIconDataString(iconData) {
+	let data = [];
+	iconData.forEach((item) => {
+		let path = '<path ';
+		Object.entries(item).forEach(([key, value]) => {
+			path += `${key}="${value}" `;
+		});
+		path += '/>';
+		data.push(path);
+	});
+	return data;
+}
+
 function appendToExports(filename) {
 	const iconFilename = _makeIconNameString(filename);
-	const exportString = _makeExportEntryString(filename, iconFilename);
+	const exportString = _makeExportEntryString(iconFilename);
 
 	console.info('* Add ' + iconFilename + ' as entry to the module exports.');
 	fs.appendFileSync(INDEX_FILE, exportString, (err) => {
@@ -79,18 +107,80 @@ function appendToExports(filename) {
 	});
 }
 
-function appendIconoirEntryToExports() {
-	const exportString = `import Iconoir from './Iconoir.svelte';
-export {Iconoir}; `;
-	console.info('\n* Add Iconoir component as entry to the module exports.');
-	fs.appendFileSync(INDEX_FILE, exportString, (err) => {
-		if (err) {
-			throw err;
-		}
+// ----------------------------------------------------------------
+
+function _toCamelCase(myStr) {
+	let text = myStr.replace(/-([a-z0-9])/g, function (g) {
+		return g[1].toUpperCase();
 	});
+	return text.replaceAll(/ /g, '');
+}
+
+function _capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function _makeIconNameString(filename) {
+	const camelCase = _toCamelCase(filename);
+
+	switch (camelCase) {
+		case '1stMedal':
+			return 'Medal1stIcon';
+			break;
+		case '360View':
+			return 'View360Icon';
+			break;
+		case '4kDisplay':
+			return 'Display4kIcon';
+			break;
+		case '4x4Cell':
+			return 'Cell4x4Icon';
+			break;
+		case 'github':
+			return 'GitHubIcon';
+		case 'githubOutline':
+			return 'GitHubOutlineIcon';
+		case 'gitlabFull':
+			return 'GitLabFullIcon';
+		case 'linkedin':
+			return 'LinkedInIcon';
+		case 'tiktok':
+			return 'TikTokIcon';
+		case 'youtube':
+			return 'YouTubeIcon';
+		default:
+			return _capitalizeFirstLetter(camelCase) + 'Icon';
+			break;
+	}
+}
+
+function _makeExportEntryString(iconFilename) {
+	return (
+		`
+	import ` +
+		iconFilename +
+		` from './iconoir/` +
+		iconFilename +
+		`.svelte';
+	export {` +
+		iconFilename +
+		`};
+	`
+	);
 }
 
 // ----------------------------------------------------------------
+function resetAll(indexFilepath, outputFolder, distFolder) {
+	_removeIndexFile(indexFilepath);
+	_removeFolder(outputFolder);
+	_removeFolder(distFolder);
+}
+
+function makeDir(pathToDir) {
+	if (!fs.existsSync(pathToDir)) {
+		fs.mkdirSync(pathToDir);
+	}
+}
 
 function _removeIndexFile(filePath) {
 	if (fs.existsSync(filePath)) {
@@ -109,57 +199,6 @@ function _removeFolder(pathToFolder) {
 	if (fs.existsSync(pathToFolder)) {
 		fs.rmSync(pathToFolder, { recursive: true, force: true });
 	}
-}
-
-function _toCamelCase(myStr) {
-	let text = myStr.replace(/-([a-z0-9])/g, function (g) {
-		return g[1].toUpperCase();
-	});
-	return text.replaceAll(/ /g, '');
-}
-
-function _capitalizeFirstLetter(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function _makeIconNameString(filename) {
-	const camelCase = _toCamelCase(filename);
-
-	switch (camelCase) {
-		case '1stMedal':
-			return 'Medal1st';
-			break;
-		case '360View':
-			return 'View360';
-			break;
-		case '4kDisplay':
-			return 'Display4k';
-			break;
-		case '4x4Cell':
-			return 'Cell4x4';
-			break;
-		case 'github':
-			return 'GitHub';
-		case 'githubOutline':
-			return 'GitHubOutline';
-		case 'gitlabFull':
-			return 'GitLabFull';
-		case 'linkedin':
-			return 'LinkedIn';
-		case 'tiktok':
-			return 'TikTok';
-		case 'youtube':
-			return 'YouTube';
-		case 'iconoir':
-			return 'IconoirIcon';
-		default:
-			return _capitalizeFirstLetter(camelCase);
-			break;
-	}
-}
-
-function _makeExportEntryString(filename, iconFilename) {
-	return 'export { default as ' + iconFilename + " } from './iconoir/" + filename + ".json';\r\n";
 }
 
 // ----------------------------------------------------------------
