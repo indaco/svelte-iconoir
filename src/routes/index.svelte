251cac83d63bf0ef740b2b600386eb006235e522
Mirco Veltri <github@mircoveltri.me>
2022-07-12 10:21:19 +0200

    docs(index.svelte): MenuIcon demo added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import { MenuIcon } from '$lib';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<MenuIcon />
