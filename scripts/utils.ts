681be1e0a769ed8f4f10700f836519a8387e5f5c
indaco <github@mircoveltri.me>
2023-10-30 15:09:57 +0100

    refactor: optimize code for scripts/utils.ts

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,3 +1,5 @@
+import type { Icon } from './index.d.ts';
+import { join } from 'node:path';
 import { promises as fsp } from 'node:fs';
 
 export function capitalizeFirstLetter(string: string) {
@@ -5,74 +7,46 @@ export function capitalizeFirstLetter(string: string) {
 }
 
 export async function mkDir(pathToDir: string, recursive = true) {
-	fsp.mkdir(pathToDir, { recursive: recursive });
+	await fsp.mkdir(pathToDir, { recursive });
 }
 
 export function relativePath(path: string) {
-	return './'.concat(path);
+	return `./${path}`;
 }
 
 export function makeComponentName(filename: string) {
-	const camelCase = toCamelCase(filename);
+	const specialCases: Record<string, string> = {
+		'1stMedal': 'Medal1stIcon',
+		'2x2Cell': 'Cell2x2Icon',
+		'3dAddHole': 'AddHole3dIcon',
+		'3dArcCenterPt': 'ArcCenterPt3dIcon',
+		'3dArc': 'Arc3dIcon',
+		'3dBridge': 'Bridge3dIcon',
+		'3dCenterBox': 'CenterBox3dIcon',
+		'3dDraftFace': 'DraftFace3dIcon',
+		'3dEllipseThreePts': 'EllipseThreePts3dIcon',
+		'3dEllipse': 'Ellipse3dIcon',
+		'3dPtBox': 'PtBox3dIcon',
+		'3dRectCornerToCorner': 'RectCornerToCorner3dIcon',
+		'3dRectFromCenter': 'RectFromCenter3dIcon',
+		'3dRectThreePts': 'RectThreePts3dIcon',
+		'3dSelectEdge': 'SelectEdge3dIcon',
+		'3dSelectFace': 'SelectFace3dIcon',
+		'3dSelectPoint': 'SelectPoint3dIcon',
+		'3dSelectSolid': 'SelectSolid3dIcon',
+		'3dThreePtsBox': 'ThreePtsBoxe3dIcon',
+		'4kDisplay': 'Display4kIcon',
+		'360View': 'View360Icon',
+		github: 'GitHubIcon',
+		githubCircle: 'GitHubCircleIcon',
+		gitlabFull: 'GitLabFullIcon',
+		linkedin: 'LinkedInIcon',
+		tiktok: 'TikTokIcon',
+		youtube: 'YouTubeIcon'
+	};
 
-	switch (camelCase) {
-		case '1stMedal':
-			return 'Medal1stIcon';
-		case '2x2Cell':
-			return 'Cell2x2Icon';
-		case '3dAddHole':
-			return 'AddHole3dIcon';
-		case '3dArcCenterPt':
-			return 'ArcCenterPt3dIcon';
-		case '3dArc':
-			return 'Arc3dIcon';
-		case '3dBridge':
-			return 'Bridge3dIcon';
-		case '3dCenterBox':
-			return 'CenterBox3dIcon';
-		case '3dDraftFace':
-			return 'DraftFace3dIcon';
-		case '3dEllipseThreePts':
-			return 'EllipseThreePts3dIcon';
-		case '3dEllipse':
-			return 'Ellipse3dIcon';
-		case '3dPtBox':
-			return 'PtBox3dIcon';
-		case '3dRectCornerToCorner':
-			return 'RectCornerToCorner3dIcon';
-		case '3dRectFromCenter':
-			return 'RectFromCenter3dIcon';
-		case '3dRectThreePts':
-			return 'RectThreePts3dIcon';
-		case '3dSelectEdge':
-			return 'SelectEdge3dIcon';
-		case '3dSelectFace':
-			return 'SelectFace3dIcon';
-		case '3dSelectPoint':
-			return 'SelectPoint3dIcon';
-		case '3dSelectSolid':
-			return 'SelectSolid3dIcon';
-		case '3dThreePtsBox':
-			return 'ThreePtsBoxe3dIcon';
-		case '4kDisplay':
-			return 'Display4kIcon';
-		case '360View':
-			return 'View360Icon';
-		case 'github':
-			return 'GitHubIcon';
-		case 'githubCircle':
-			return 'GitHubCircleIcon';
-		case 'gitlabFull':
-			return 'GitLabFullIcon';
-		case 'linkedin':
-			return 'LinkedInIcon';
-		case 'tiktok':
-			return 'TikTokIcon';
-		case 'youtube':
-			return 'YouTubeIcon';
-		default:
-			return capitalizeFirstLetter(camelCase) + 'Icon';
-	}
+	const specialCase = specialCases[toCamelCase(filename)];
+	return specialCase ? specialCase : `${toPascalCase(filename)}Icon`;
 }
 
 export function makeComponentFilename(component: string): string {
@@ -83,7 +57,31 @@ export function makeComponentDTSFilename(component: string): string {
 	return makeComponentFilename(component).concat('.d.ts');
 }
 
+/**
+ * It writes a file called `index.ts` in the folder for each icon, which exports the icon component.
+ *
+ * @param {string} outputFolder - The folder where the icon component will be created.
+ * @param {Icon} iconObj - The icon object from the icons.json file.
+ */
+export async function generateIconComponentIndex(
+	outputFolder: string,
+	iconObj: Icon
+): Promise<void> {
+	const txt = `export { default as ${iconObj.component} } from './${iconObj.componentFile}';`;
+
+	await fsp.writeFile(join(outputFolder, iconObj.componentFolder, 'index.ts'), txt);
+}
+
+export function generateExportEntryString(iconObj: Icon): string {
+	return `export { ${iconObj.component} } from './${iconObj.componentFolder}/index.js';\n`;
+}
+
 // ----------------------------------------------------------------
+
+function toPascalCase(str: string) {
+	const text = str.replace(/(?:^|-)([a-z0-9])/g, (_, char) => char.toUpperCase());
+	return text.replace(/ /g, '');
+}
 
 function toCamelCase(str: string) {
 	const text = str.replace(/-([a-z0-9])/g, (g) => g[1].toUpperCase());
