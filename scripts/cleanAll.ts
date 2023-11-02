6a2efcb5c6b1ecbe8b55a4931e204ebc6487ced1
indaco <github@mircoveltri.me>
2023-11-02 17:07:57 +0100

    refactor: icon components use the new IconBase component

--- scripts/cleanAll.ts
+++ scripts/cleanAll.ts
@@ -8,12 +8,13 @@ import pc from 'picocolors';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const LIB_FOLDER = join(__dirname, '..', '..', 'src', 'lib');
+const ICONS_FOLDER = join(LIB_FOLDER, 'icons');
 
 // ----------------------------------------------------------------
 
 const removeLibFolder = async () => {
 	try {
-		await fs.rm(LIB_FOLDER, { recursive: true, force: true });
+		await fs.rm(ICONS_FOLDER, { recursive: true, force: true });
 		console.log(pc.blue('* Successfully removed previously generated components.'));
 	} catch (err) {
 		console.error(err);
---
+++ src/lib/IconBase.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import type { IconSize, IconSizeMap, SVGProps } from './Icon.d.ts';
+
+	const sizeMap = {
+		xs: '1em',
+		sm: '1.25em',
+		base: '1.5em',
+		lg: '1.75em',
+		xl: '2em'
+	} satisfies IconSizeMap;
+
+	interface $$Props extends SVGProps {}
+
+	export let name: $$Props['name'];
+	export let altText: $$Props['altText'] = undefined;
+	export let size: $$Props['size'] = 'base';
+
+	const defaultSize = sizeMap['base'];
+
+	let _altText = altText ?? `${name} icon`;
+
+	let _size =
+		size in sizeMap
+			? sizeMap[size as unknown as IconSize]
+			: typeof size === 'number' || typeof size === 'string'
+			? size
+			: defaultSize;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={_size}
+	height={_size}
+	fill="none"
+	stroke-width="1.5"
+	viewBox="0 0 24 24"
+	aria-hidden="true"
+	aria-labelledby={_altText}
+	{...$$restProps}
+	on:click
+	on:dblclick
+	on:keydown
+	on:keyup
+	on:mouseenter
+	on:mouseleave
+>
+	<slot />
+</svg>
